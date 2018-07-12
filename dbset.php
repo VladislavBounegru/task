cafcd20ee4cab6490a387070e9ca9383e14231cb
wlncent <36441693+wlncent@users.noreply.github.com>
2018-07-12 18:55:24 +0300

    Update dbset.php

--- dbset.php
+++ dbset.php
@@ -6,6 +6,6 @@ $dbname = "tsk";
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-	mysqli_query($conn,"SET names 'cp1251'");
-	mysqli_query($conn,"SET NAMES utf8");
-} ?>
+    mysqli_query($conn,"SET names 'cp1251'");
+    mysqli_query($conn,"SET NAMES utf8");
+}
