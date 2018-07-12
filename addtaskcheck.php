4e7ff9ff950d180a0ab30467003d619eb4f17c0a
wlncent <36441693+wlncent@users.noreply.github.com>
2018-07-12 18:54:33 +0300

    Update addtaskcheck.php

--- addtaskcheck.php
+++ addtaskcheck.php
@@ -1,17 +1,16 @@
 <?php
 session_start();
-include "dbset.php";
-$suid=$_SESSION["suid"];
-$slogin=$_SESSION["slogin"];
+include 'dbset.php';
+$suid = $_SESSION['suid'];
+$slogin = $_SESSION['slogin'];
 
-if (!isset($_POST["task"]) || ($_POST["task"]==""))
-{
-	header("Location: addtask.php?err=emptylog");
-exit;}
-$task=$_POST["task"];
-include "dbset.php";
-$query="INSERT INTO tasks (goal,id) VALUES ('".$task."','".$suid."')";
-$res=mysqli_query($conn,$query);
-header("Location: addtask.php?add=ok");
-		exit;
-?>
+if (! isset( $_POST["task"] ) || ($_POST["task"]=="")) {
+    header('Location: addtask.php?err=emptylog');
+    exit;
+}
+$task = $_POST["task"];
+
+$query = "INSERT INTO `tasks` (`goal`,`id`) VALUES ('" . $task . "','" . $suid . "')";
+$res = mysqli_query($conn,$query);
+header('Location: addtask.php?add=ok');
+exit;
