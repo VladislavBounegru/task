f6f40c7eecc313f2a7b34005ffbf26e36fabf766
wlncent <36441693+wlncent@users.noreply.github.com>
2018-07-12 19:48:04 +0300

    Update user_check.php

--- user_check.php
+++ user_check.php
@@ -1,35 +1,43 @@
 <?php
 session_start();
-if (!isset($_POST["login"])) {
-	header("Location: main.php?err=1");
-exit;}
-if ( !isset($_POST["pasw"])) {
-	header("Location: main.php?err=2");
-exit;}
-if ($_POST["login"]=="") {
-	header("Location: main.php?err=3");
-exit;}
-if ($_POST["pasw"]=="") {
-	header("Location: main.php?err=4");
-exit;}
-if (($_POST["login"]=="") && ($_POST["pasw"]=="")) {
-	header("Location: main.php?err=5");
-exit;}
-// $_POST["lgn"]-пусто-->err=151
-// $_POST["psw"]==""-->err=152
-//----
-$login=$_POST["login"];
-$pasw=$_POST["pasw"];
-include "dbset.php";
-$query="SELECT * FROM user WHERE login='".$login."' and pass='".$pasw."'";
-$res=mysqli_query($conn,$query) or die("ERR:query");
-$nr=mysqli_num_rows($res);
-if($nr==0) {header("Location:main.php?err=25");exit;}
-if($nr>1) {header("Location:main?err=26");exit;}
+if (! isset ($_POST["login"])) {
+    header("Location: main.php?err=1");
+    exit;
+}
+if (! isset ($_POST["pasw"])) {
+    header("Location: main.php?err=2");
+    exit;
+}
+if ($_POST["login"] == "") {
+    header("Location: main.php?err=3");
+    exit;
+}
+if ($_POST["pasw"] == "") {
+    header("Location: main.php?err=4");
+    exit;
+}
+if (($_POST["login"] == "") && ($_POST["pasw"] == "")) {
+    header("Location: main.php?err=5");
+    exit;
+}
+$login = $_POST["login"];
+$pasw = $_POST["pasw"];
+include 'dbset.php';
+$query="SELECT * FROM `user` WHERE `login`='" . $login . "' and pass='" . $pasw . "'";
+$res = mysqli_query($conn,$query) or die("ERR:query");
+$nr = mysqli_num_rows($res);
+if ($nr == 0) {
+    header("Location:main.php?err=25");
+    exit;
+}
+if ($nr > 1) {
+    header("Location:main?err=26");
+    exit;
+}
 
-$row=mysqli_fetch_array($res);
-$_SESSION["suid"]=$row["id"];
-$_SESSION["slogin"]=$login;
+$row = mysqli_fetch_array($res);
+$_SESSION["suid"] = $row["id"];
+$_SESSION["slogin"] = $login;
 header("Location:lk.php");
 exit;
 ?>
